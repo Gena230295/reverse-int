fa0c2a1c94a0e9bc5230c134eeaa6dac2c355187
Gena230295 <gena23solov@gmail.com>
2024-08-19 10:14:47 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const str = Math.abs(n).toString();
+    const arr = str.split("").reverse().join("");
+    const num = Number(arr);
+    return num;
+};
